4d6d7295141fa4d8dd72309951e71b8d64a09b52
Merlin Chatou <merlin.chatou@gmail.com>
2025-05-22 22:53:22 +0200

    Prepare documentation modal

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
 import hljs from "highlight.js";
 import '@fortawesome/fontawesome-free/js/all.js';
 import "./color-scheme-switcher";
-import { toggleModal } from "@btfcss/modal";
+//import "@btfcss/modal";
+import "./modal";
 
 
 
@@ -13,7 +14,24 @@ hljs.highlightAll({ showLanguageLabel: true });
 
 // The page is hidden at startup
 // Display the page once loaded
-window.onload = function () {  
+window.onload = function () {
   document.querySelector('body').style.removeProperty('display');
 };
 
+
+
+// Prepare documentation modal elements
+const documentationModal = document.getElementById('documentation-modal');
+const documentationModalHeader = documentationModal.querySelector('header > h2');
+const documentationModalContent = documentationModal.querySelector('.modal-content > div');
+
+
+// When the modal documentation is opened
+documentationModal.addEventListener('onModalShow', (event) => {
+    
+  // Populate them modal
+    const documentationId = 'doc-' + event.triggerElement.dataset.documentation;
+    const doc = document.getElementById(documentationId);
+    documentationModalHeader.innerHTML = doc.querySelector('h2').innerHTML;
+    documentationModalContent.innerHTML = doc.querySelector('content').innerHTML;
+});
